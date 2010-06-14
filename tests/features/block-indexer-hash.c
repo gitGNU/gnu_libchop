a6a0bc33538f594b288662457fd6f52228aa9a34
Ludovic Courtès <ludo@gnu.org>
2010-06-14 22:49:43 +0200

    Generalize the `block-indexer-hash' test.
    * tests/features/block-indexer-hash.c (test_index_and_fetch): New
      INDEXER and HASH_METHOD arguments.  Remove INDEXER's instantiation.
      Attach FETCHER's log when in debug mode.  Adjust to match HASH_METHOD
      instead of MD5.
      (main)[block_indexer_classes, block_indexer_serials]: New variables.
      Iterate over them.

--- tests/features/block-indexer-hash.c
+++ tests/features/block-indexer-hash.c
@@ -20,6 +20,7 @@
 #include <chop/chop-config.h>
 
 #include <alloca.h>
+#include <string.h>
 
 #include <chop/chop.h>
 #include <chop/stores.h>
@@ -29,42 +30,45 @@
 
 
 static void
-test_index_and_fetch (chop_block_store_t *store)
+test_index_and_fetch (chop_block_store_t *store,
+		      chop_block_indexer_t *indexer)
 {
   static char data[7777];
 
   chop_error_t err;
-  chop_block_indexer_t *indexer;
   chop_block_fetcher_t *fetcher;
   chop_index_handle_t *handle;
-  char hash[chop_hash_size (CHOP_HASH_MD5)];
-  chop_block_key_t key;
+  chop_block_iterator_t *iterator;
   chop_buffer_t buffer;
   size_t size;
+  char *p;
 
   test_randomize_input (data, sizeof (data));
 
   /* Prepare.  */
-  indexer = chop_class_alloca_instance (&chop_hash_block_indexer_class);
-
-  err = chop_hash_block_indexer_open (CHOP_HASH_MD5, indexer);
-  test_check_errcode (err, "opening hash block indexer");
-
   fetcher = chop_block_indexer_alloca_fetcher (indexer);
   err = chop_block_indexer_initialize_fetcher (indexer, fetcher);
   test_check_errcode (err, "initializing hash block fetcher");
 
+  if (test_debug_mode ())
+    {
+      chop_log_t *log;
+
+      log = chop_hash_block_fetcher_log (fetcher);
+      if (log != NULL)
+	chop_log_attach (log, 2, 0);
+    }
+
   handle = chop_block_indexer_alloca_index_handle (indexer);
 
   /* Index.  */
-  test_stage ("indexing");
+  test_stage_intermediate ("indexing");
   err = chop_block_indexer_index (indexer, store, data, sizeof (data),
 				  handle);
   test_check_errcode (err, "indexing block");
-  test_stage_result (1);
 
   /* Fetch.  */
-  test_stage ("fetching unaltered block");
+  test_stage_intermediate ("fetching unaltered block");
   err = chop_buffer_init (&buffer, sizeof (data));
   test_check_errcode (err, "initializing buffer");
 
@@ -76,18 +80,25 @@ test_index_and_fetch (chop_block_store_t *store)
   test_assert (size == sizeof (data));
   test_assert (size == chop_buffer_size (&buffer));
   test_assert (!memcmp (data, chop_buffer_content (&buffer), size));
-  test_stage_result (1);
 
   chop_buffer_clear (&buffer);
 
-  /* Alter.  */
-  test_stage ("integrity checks");
-  test_stage_intermediate ("tampering with block");
-  chop_hash_buffer (CHOP_HASH_MD5, data, sizeof (data), hash);
-  chop_block_key_init (&key, hash, chop_hash_size (CHOP_HASH_MD5), NULL, NULL);
+  /* Get the first block written to STORE.  */
+  iterator = chop_class_alloca_instance (chop_store_iterator_class (store));
+  err = chop_store_first_block (store, iterator);
+  test_check_errcode (err, "getting iterator to first block");
 
-  data[0] = data[0] + 1;
-  err = chop_store_write_block (store, &key, data, sizeof (data));
+  err = chop_store_read_block (store, chop_block_iterator_key (iterator),
+			       &buffer, &size);
+  test_check_errcode (err, "reading block");
+
+  /* Alter it.  */
+  test_stage_intermediate ("tampering with block");
+  p = (char *) chop_buffer_content (&buffer);
+  p[0] = p[0] + 1;
+  err = chop_store_write_block (store, chop_block_iterator_key (iterator),
+				chop_buffer_content (&buffer),
+				chop_buffer_size (&buffer));
   test_check_errcode (err, "writing altered block");
 
   /* Fetch and make sure the alteration doesn't go unnoticed.  */
@@ -95,38 +106,97 @@ test_index_and_fetch (chop_block_store_t *store)
   err = chop_block_fetcher_fetch (fetcher, handle, store, &buffer, &size);
   test_assert (err != 0);
   test_assert (size == 0);
-  test_stage_result (1);
 
   chop_buffer_return (&buffer);
+  chop_object_destroy ((chop_object_t *) iterator);
   chop_object_destroy ((chop_object_t *) fetcher);
-  chop_object_destroy ((chop_object_t *) indexer);
 }
 
 
 int
 main (int argc, char *argv[])
 {
+  /* The block indexers.  */
+  static const chop_class_t *block_indexer_classes[] =
+    {
+      &chop_hash_block_indexer_class,
+      &chop_hash_block_indexer_class,
+      &chop_hash_block_indexer_class,
+      &chop_hash_block_indexer_class,
+      NULL
+    };
+
+  static const char *block_indexer_serials[] =
+    {
+      "md5",
+      "sha1",
+      "rmd160",
+      "sha256",
+      NULL
+    };
+
   static const char db_file[] = ",,block-indexer-hash.db";
 
   chop_error_t err;
   chop_block_store_t *store;
+  const chop_class_t **indexer_class;
+  const char **indexer_serial;
 
   test_init (argv[0]);
 
   err = chop_init ();
   test_check_errcode (err, "initializing libchop");
 
-  remove (db_file);
-
   store =
     chop_class_alloca_instance ((chop_class_t *) &chop_gdbm_block_store_class);
-  err = chop_gdbm_store_open (db_file, 0, O_RDWR | O_CREAT,
-			      S_IRUSR | S_IWUSR, NULL, store);
-  test_check_errcode (err, "opening block store");
 
-  test_index_and_fetch (store);
+  for (indexer_class = block_indexer_classes,
+	 indexer_serial = block_indexer_serials;
+       *indexer_class != NULL;
+       indexer_class++, indexer_serial++)
+    {
+      size_t consumed;
+      chop_block_indexer_t *indexer;
 
-  chop_object_destroy ((chop_object_t *) store);
+      test_stage ("`%s', `%s'", chop_class_name (*indexer_class),
+		  *indexer_serial);
+
+      remove (db_file);
+
+      /* Open a fresh block store.  */
+      err = chop_gdbm_store_open (db_file, 0, O_RDWR | O_CREAT,
+				  S_IRUSR | S_IWUSR, NULL, store);
+      test_check_errcode (err, "opening block store");
+
+      if (test_debug_mode ())
+	{
+	  chop_block_store_t *debugger;
+
+	  debugger = chop_class_alloca_instance (&chop_dummy_block_store_class);
+	  chop_dummy_proxy_block_store_open ("store", store, debugger);
+	  chop_log_attach (chop_dummy_block_store_log (debugger), 2, 0);
+	  store = debugger;
+	}
+
+      /* Initialize the block indexer.  */
+      indexer = chop_class_alloca_instance (*indexer_class);
+      err = chop_object_deserialize ((chop_object_t *) indexer,
+				     *indexer_class, CHOP_SERIAL_ASCII,
+				     *indexer_serial,
+				     strlen (*indexer_serial),
+				     &consumed);
+      test_check_errcode (err, "deserializing block indexer");
+      test_assert (consumed == strlen (*indexer_serial));
+
+      test_index_and_fetch (store, indexer);
+
+      chop_object_destroy ((chop_object_t *) indexer);
+
+      chop_store_close (store);
+      chop_object_destroy ((chop_object_t *) store);
+
+      test_stage_result (1);
+    }
 
   remove (db_file);
 
