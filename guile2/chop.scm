16bc6ef72f9550f1971fa71cc55895f953c25471
Ludovic Courtès <ludo@gnu.org>
2011-03-31 22:45:45 +0200

    guile2: Add (chop).
    * guile2/chop.scm: New file.
    * guile2/Makefile.am (SOURCES): Add `chop.scm'.

---
+++ guile2/chop.scm
@@ -0,0 +1,36 @@
+;;; Copyright (C) 2011  Ludovic Courtès <ludo@gnu.org>
+;;;
+;;; Libchop is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; Libchop is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with libchop.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (chop))
+
+;; The composite module that re-exports everything from the public modules.
+
+(eval-when (eval load compile)
+  (begin
+    (define %public-modules
+      '(block-indexers
+        choppers
+        cipher
+        core
+        hash
+        indexers
+        objects
+        stores
+        streams))
+
+    (for-each (let ((i (module-public-interface (current-module))))
+                (lambda (m)
+                  (module-use! i (resolve-interface `(chop ,m)))))
+              %public-modules)))
